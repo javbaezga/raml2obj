915649ec384286c15f8c98d613b043423a44ae86
Pawel Psztyc <jarrodek@gmail.com>
2017-07-26 00:57:15 +0100

    Fix: Fixes https://github.com/mulesoft/api-console/issues/136

---
+++ test/ac-issue-136.spec.js
@@ -0,0 +1,44 @@
+/* eslint-env node, mocha */
+
+const raml2obj = require('..');
+const assert = require('assert');
+const parser = require('./parser');
+
+describe('raml2obj', () => {
+  describe('array-of-foo.raml', () => {
+    let obj;
+
+    before(() => {
+      return parser('test/ac-issue-136.raml')
+      .then(result => raml2obj.parse(result))
+      .then((result) => {
+        obj = result;
+      });
+    });
+
+    it('baseUriParameters should be as defined in RAML', () => {
+      const param = obj.baseUriParameters[1];
+      assert.equal(param.enum[0], 'v1');
+    });
+
+    it('/resource should have original enum value', () => {
+      const param = obj.resources[0].allUriParameters[1];
+      assert.equal(param.enum[0], 'v1');
+    });
+
+    it('GET /resource should have original enum value', () => {
+      const param = obj.resources[0].methods[0].allUriParameters[1];
+      assert.equal(param.enum[0], 'v1');
+    });
+
+    it('/aggregate/contents should have new enum value', () => {
+      const param = obj.resources[1].allUriParameters[1];
+      assert.equal(param.enum[0], 'v2');
+    });
+
+    it('GET /aggregate/contents should have new enum value', () => {
+      const param = obj.resources[1].methods[0].allUriParameters[1];
+      assert.equal(param.enum[0], 'v2');
+    });
+  });
+});
