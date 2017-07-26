6db8d59bc31da4185d4d147ac6f9fd774e0eaf08
Pawel Psztyc <jarrodek@gmail.com>
2017-07-26 22:02:35 +0100

    Temporary fix for https://github.com/raml-org/raml-js-parser-2/issues/779

---
+++ test/fv-359.spec.js
@@ -0,0 +1,31 @@
+/* eslint-env node, mocha */
+
+const raml2obj = require('..');
+const assert = require('assert');
+const parser = require('./parser');
+
+describe('raml2obj', () => {
+  describe('jira issue fv-359', () => {
+    let obj;
+
+    before(() => {
+      return parser('test/fv-359/dev.raml')
+      .then(result => raml2obj.parse(result))
+      .then((result) => {
+        obj = result;
+      });
+    });
+
+    it('baseUri should be extended', () => {
+      assert.equal(obj.baseUri, 'http://devx.example.com');
+    });
+
+    it('resource base uri should be extended', () => {
+      assert.equal(obj.resources[0].absoluteUri, 'http://devx.example.com/resource');
+    });
+
+    it('method base uri should be extended', () => {
+      assert.equal(obj.resources[0].methods[0].absoluteUri, 'http://devx.example.com/resource');
+    });
+  });
+});
