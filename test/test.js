06baa412abb9d1d1ae05a2e3554c6e7d8d0a380a
Pawel Psztyc <jarrodek@gmail.com>
2017-07-26 22:11:07 +0100

    Fix: Fixed variable name.

--- test/test.js
+++ test/test.js
@@ -1,20 +1,20 @@
 const parser = require('./parser');
 const raml2obj = require('..');
-const fs = require('fs');
+// const fs = require('fs');
 
 // const apiUrl = 'test/address-1.0.0-raml-fragment/address.raml';
 // const apiUrl = 'test/employment-1.0.0-raml-fragment/employment.raml';
 // const apiUrl = 'test/raml-example-api-master/api.raml';
-const apiUrl = 'test/fv-359/dev.raml';
+const apiUrl = 'test/parameters.raml';
 parser(apiUrl)
 .then(result => {
   return raml2obj.parse(result);
 })
-.then(result => {
-  const jsonString = JSON.stringify(result, null, 2);
-  const filename = 'test/fv-359.json';
-  fs.writeFileSync(filename, jsonString);
-})
+// .then(result => {
+//   const jsonString = JSON.stringify(result, null, 2);
+//   const filename = 'test/fv-359.json';
+//   fs.writeFileSync(filename, jsonString);
+// })
 .catch((data) => {
   console.error(data);
 });
