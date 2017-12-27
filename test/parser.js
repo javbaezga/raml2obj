c818266652b376c5c328f77cc8a795280f97bae1
Pawel Psztyc <jarrodek@gmail.com>
2017-12-27 16:02:31 +0100

    Fix: Fixes types normalization when type is included in line.

--- test/parser.js
+++ test/parser.js
@@ -2,10 +2,12 @@ const raml = require('raml-1-parser');
 
 module.exports = function(source) {
   return raml
-  .loadApi(source, {
-    rejectOnErrors: false
-  })
-  .then(result => {
-    return result.expand(true).toJSON({serializeMetadata: false});
-  });
+    .loadApi(source, {
+      rejectOnErrors: false
+    })
+    .then(result => {
+      return result.expand(true).toJSON({
+        serializeMetadata: false
+      });
+    });
 };
--- test/test.js
+++ test/test.js
@@ -5,15 +5,16 @@ const fs = require('fs');
 // const apiUrl = 'test/address-1.0.0-raml-fragment/address.raml';
 // const apiUrl = 'test/employment-1.0.0-raml-fragment/employment.raml';
 // const apiUrl = 'test/raml-example-api-master/api.raml';
-const apiUrl = 'test/array-of-foo.raml';
+// const apiUrl = 'test/array-of-foo.raml';
+const apiUrl = 'test/inline-types.raml';
 parser(apiUrl)
 .then(result => {
   debugger;
   return raml2obj.parse({json: result});
 })
 .then(result => {
-  const jsonString = JSON.stringify(result, null, 2);
-  const filename = 'test/raml-example-api.json';
+  const jsonString = JSON.stringify(result.json, null, 2);
+  const filename = 'test/inline-types.json';
   fs.writeFileSync(filename, jsonString);
 })
 .catch((data) => {
